200e0071e76f3d45feb2dc2915163f9682e9a681
lucaGomezB <gomez.luca.007@gmail.com>
2025-05-24 19:36:00 -0300

    Primer commit

---
+++ PIElBuenSabor/src/main/java/com/lucagomezb/pielbuensabor/PIElBuenSabor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lucagomezb.pielbuensabor;
+
+/**
+ *
+ * @author lucaGomezB <lucaGomezB at https://lucagomezb.github.io/Luca-Gomez/>
+ */
+public class PIElBuenSabor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
